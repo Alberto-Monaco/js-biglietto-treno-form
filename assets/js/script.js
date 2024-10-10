d1f3099e1c6c58def1a74542ffe40a3810ff3cab
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-10 10:43:15 +0200

    fix parsefloat

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch'essa da scrivere in console.
 MILESTONE 2: Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 Nota: Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
 Nota 2:
@@ -76,7 +76,7 @@ FormEl.addEventListener('submit', function (e) {
 		total = final_price
 		message = 'e non hai diritto a sconti'
 	}
-	total_around = Number(total.toFixed(2))
+	total_around = parseFloat(total.toFixed(2))
 
 	console.log(`Il totale che pagherai è € ${total_around} ${message}`)
 
